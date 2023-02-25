4b675134ad4b105fb1c6e1ca9243997c4ca09cd7
Maulana Malik Ibrahim <maulana.ibrahim@laku6.com>
2023-02-25 17:07:29 +0700

    added condition for framing
    - send picture if the combination of image and frame already made
    - put insert new framedimage in separate function
    - added more error handling

--- controllers/frameEngineController.go
+++ controllers/frameEngineController.go
@@ -4,7 +4,7 @@ import (
 	"encoding/base64"
 	"go-framing-engine/initializer"
 	"go-framing-engine/models"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"strconv"
 
@@ -12,97 +12,145 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type RequestBody struct {
+	ImageUrl    string
+	FrameUrl    string
+	Settings    Settings
+}
 
-func FrameImages (c *gin.Context) {
-	var body struct {
-		ImageUrl string
-		FrameUrl string
-		FramedImage string
-		ImageSize []int
-		FrameSize []int
-		ImagePosition []int
-	}
+type Settings struct {
+	ImageWidth  int
+	ImageHeight int
+	FrameWidth  int
+	FrameHeight int
+	ImagePosX   int
+	ImagePosY   int
+}
 
-	c.Bind(&body)
-	
-	inputImage := getInputImage(body.ImageUrl)
-	frameImage := getFrameImage(body.FrameUrl)	
 
-	// Resize the frame image to match the size of the input image
-	frameImage.Thumbnail(body.FrameSize[0],body.FrameSize[1], vips.InterestingNone)
-	inputImage.Thumbnail(body.ImageSize[0],body.ImageSize[0], vips.InterestingNone)
-
-	// Combine the input image and the frame image using the composite function
-	frameImage.Composite(inputImage, vips.BlendModeDestOver,body.ImagePosition[0],body.ImagePosition[0])	
-
-	// Export image to jpeg
-	out, _, err := frameImage.ExportJpeg(nil)
-	if err != nil {
-		panic(err)
-	}
-	
-	// if we want to write the file to local
-	// _, err = os.Create(fmt.Sprintf("new_image-%v.jpeg", time.Now().Unix()))
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// outFile.Write(out)
-
-	// Encode image to base64 then insert to db
-	encodedImage := base64.StdEncoding.EncodeToString(out)
-	insertFramedImage := models.FramedImages{ImageUrl: body.ImageUrl, FrameUrl: body.FrameUrl, FramedImage: encodedImage, ImageSize: strconv.Itoa(body.ImageSize[0])+","+strconv.Itoa(body.ImageSize[1]),FrameSize: strconv.Itoa(body.FrameSize[0])+","+strconv.Itoa(body.FrameSize[1]),ImagePosition: strconv.Itoa(body.ImagePosition[0])+","+strconv.Itoa(body.ImagePosition[1])}
-	result := initializer.DB.Create(&insertFramedImage) // pass pointer of data to Create
-	if result.Error != nil {
-		c.Status(400)
+func FrameImages(c *gin.Context) {
+	// Get the request body
+	var reqBody RequestBody
+	if err := c.BindJSON(&reqBody); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	// Set response headers
-	c.Header("Content-Type", "image/jpeg")
+	// Check if the combination of image and frame already made before
+	var framedImage models.FramedImages
+	if err := initializer.DB.Where(&models.FramedImages{ImageUrl: reqBody.ImageUrl, FrameUrl: reqBody.FrameUrl}).First(&framedImage).Error; err != nil {
+		// no matching record found, create a new one
+		// Encode image from url to vips image
+		inputImage, err := getInputImage(reqBody.ImageUrl)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		frameImage, err := getFrameImage(reqBody.FrameUrl)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
 
-	// Write image data to response body
-	c.Data(http.StatusOK, "image/jpeg", out)
+		// Resize the frame image to match the size of the input image
+		frameImage.Thumbnail(reqBody.Settings.FrameWidth,reqBody.Settings.FrameHeight, vips.InterestingNone)
+		inputImage.Thumbnail(reqBody.Settings.ImageWidth,reqBody.Settings.ImageHeight, vips.InterestingNone)
 
-	// Set status
-	c.Status(http.StatusOK)
+		// Combine the input image and the frame image using the composite function
+		frameImage.Composite(inputImage, vips.BlendModeDestOver,reqBody.Settings.ImagePosX,reqBody.Settings.ImagePosY)
+
+		// Export image to jpeg	
+		out, _, err := frameImage.ExportJpeg(nil)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Insert the framed image into the database
+		if err := insertFramedImage(reqBody, out); err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Set response headers,status and Write image data to response body
+		c.Header("Content-Type", "image/jpeg")
+		c.Data(http.StatusOK, "image/jpeg", out)
+		c.Status(http.StatusOK)
+	} else {
+		// matching record found, send the image
+		// Decode the base64 to image
+		imgData, err := base64.StdEncoding.DecodeString(framedImage.FramedImage)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Set response headers,status and Write image data to response body
+		c.Header("Content-Type", "image/jpeg")
+		c.Data(http.StatusOK, "image/jpeg", imgData)
+		c.Status(http.StatusOK)
+	}	
 }
 
-func getInputImage(imageUrl string) *vips.ImageRef {
-	resp, err := http.Get(imageUrl)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
+func insertFramedImage(reqBody RequestBody, out []byte) error {
+	// Encode image to base64
+	encodedImage := base64.StdEncoding.EncodeToString(out)
 
-	inputData, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	inputImage, err := vips.NewImageFromBuffer(inputData)
-	if err != nil {
-		panic(err)
+	// Create a new FramedImages record
+	insertFramedImage := models.FramedImages{
+		ImageUrl:      reqBody.ImageUrl,
+		FrameUrl:      reqBody.FrameUrl,
+		FramedImage:   encodedImage,
+		ImageSize:     strconv.Itoa(reqBody.Settings.ImageWidth) + "," + strconv.Itoa(reqBody.Settings.ImageHeight),
+		FrameSize:     strconv.Itoa(reqBody.Settings.FrameWidth) + "," + strconv.Itoa(reqBody.Settings.FrameHeight),
+		ImagePosition: strconv.Itoa(reqBody.Settings.ImagePosX) + "," + strconv.Itoa(reqBody.Settings.ImagePosY),
 	}
 
-	return inputImage
+	// Insert the record into the database
+	result := initializer.DB.Create(&insertFramedImage) // pass pointer of data to Create
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
 }
 
-func getFrameImage(frameUrl string) *vips.ImageRef {
+func getInputImage(imageUrl string) (*vips.ImageRef, error) {
+    resp, err := http.Get(imageUrl)
+    if err != nil {
+        return nil, err
+    }
+    defer resp.Body.Close()
+
+    inputData, err := io.ReadAll(resp.Body)
+    if err != nil {
+        return nil, err
+    }
+
+    inputImage, err := vips.NewImageFromBuffer(inputData)
+    if err != nil {
+        return nil, err
+    }
+
+    return inputImage, nil
+}
+
+func getFrameImage(frameUrl string) (*vips.ImageRef, error) {
 	resp, err := http.Get(frameUrl)
 	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
+        return nil, err
+    }
+    defer resp.Body.Close()
 
-	inputData, err := ioutil.ReadAll(resp.Body)
+	inputData, err := io.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
-	}
+        return nil, err
+    }
 
 	frameImage, err := vips.NewImageFromBuffer(inputData)
 	if err != nil {
-		panic(err)
-	}
+        return nil, err
+    }
 
-	return frameImage
+	return frameImage, nil
 }
